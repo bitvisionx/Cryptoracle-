ad56203f2dd4642e9ed760b1e239a93d994b6433
Steven De wolf <stevendewolf1983@icloud.com>
2025-05-18 16:51:24 +0200

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>CrypVisionX</h1>
+      <h2>Cryptocurrency Voorspellingen</h2>
+    </header>
+  );
+}
